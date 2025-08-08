0f153e12a940fcfa0af6a4755e6c1b7a596d8d1b
Aakash <suryapadayachi58@gmail.com>
2025-08-08 10:03:56 +0000

    make the image in hero section from bottom right corner curver

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -102,7 +102,7 @@ export function HeroSection() {
         <div className="relative flex items-center justify-center bg-secondary/20 p-0 md:p-0 overflow-hidden">
              {/* Image */}
              <div className="absolute top-0 right-[7.5%] h-[90%] w-[85%]">
-                <div className="relative h-full w-full rounded-bl-[80px] overflow-hidden">
+                <div className="relative h-full w-full rounded-br-[80px] overflow-hidden">
                     <div className="absolute inset-0 bg-background opacity-50 z-10"></div>
                      <Image src="/assets/profile-hero.jpg" data-ai-hint="man portrait" alt="Bharath Naidu" fill className="object-cover" />
                 </div>
